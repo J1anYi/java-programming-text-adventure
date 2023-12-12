e1cf14dec912e9a1cd4629dbd881a4326bb315c7
j1anyi <pp517462915@gmail.com>
2023-12-12 14:44:43 +0100

    GameRepository add goods, armors, weapons and monsters

--- src/main/java/model/goods/Armor.java
+++ src/main/java/model/goods/Armor.java
@@ -1,4 +1,33 @@
 package model.goods;
 
-public class Armor {
+public class Armor extends Goods implements Cloneable{
+
+    private int defensePower;
+
+    public Armor() {}
+
+    public Armor(String description, int amount, int defensePower) {
+        super(description, amount);
+        this.defensePower = defensePower;
+    }
+
+    public int getDefensePower() {
+        return defensePower;
+    }
+
+    public void setDefensePower(int defensePower) {
+        this.defensePower = defensePower;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Armor{" +
+                "defensePower=" + defensePower +
+                '}';
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+    	return super.clone();
+    }
 }
--- src/main/java/model/goods/Goods.java
+++ src/main/java/model/goods/Goods.java
@@ -1,4 +1,50 @@
 package model.goods;
 
-public class Goods {
+
+/**
+ * Goods is a superclass of Weapon and Armor
+ * in order to represent the goods in the game
+ * decrption: String
+ * amount: Integer
+ */
+public class Goods implements Cloneable{
+
+    private String description;
+    private int amount;
+
+    public Goods() {}
+
+    public Goods(String description, int amount) {
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Goods{" +
+                "description='" + description + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+    	return super.clone();
+    }
 }
